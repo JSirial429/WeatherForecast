9de42ba0a09cf29fc274de4f6808ce4a8a8f6a9c
JSirial429 <Jos0928@gmail.com>
2025-11-29 09:39:04 -0500

    Adding new tabs file view

---
+++ WeatherForecast/TabsMenu.swift
@@ -0,0 +1,18 @@
+//
+//  TabsMenu.swift
+//  WeatherForecast
+//
+//  Created by Jorge Sirias on 11/29/25.
+//
+
+import SwiftUI
+
+struct TabsMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabsMenu()
+}
